4c79210eb09787ea0c9a984ddc694cdcd5111e9f
victorsmits <victor.smits@student.vinci.be>
2018-10-04 12:52:46 +0200

    first commit

---
+++ gestion_evaluation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gestion_evaluation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
